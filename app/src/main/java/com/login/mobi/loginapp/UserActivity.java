fd960abf775ac9c48bb0105cc04f3e3a7aee1309
kikichau <kiki.1155@hotmail.com>
2019-04-23 16:58:17 +0800

    Correct error

--- app/src/main/java/com/login/mobi/loginapp/UserActivity.java
+++ app/src/main/java/com/login/mobi/loginapp/UserActivity.java
@@ -1,12 +1,15 @@
 package com.login.mobi.loginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
     private TextView tvUser;
+    private Button btDiaryIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +17,19 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
         tvUser = findViewById(R.id.tvUser);
+        btDiaryIn = findViewById(R.id.btDiaryIn);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             tvUser.setText(bundle.getString("user"));
         }
+
+        btDiaryIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserActivity.this, DiaryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
